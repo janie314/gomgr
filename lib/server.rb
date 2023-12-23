fd966d6abadfa74d0838b40f66a49001ff8afed4
12ball <pub@janie.page>
2023-12-22 21:23:39 -0600

    1703301819

--- lib/server.rb
+++ lib/server.rb
@@ -4,7 +4,18 @@ require "semantic_logger"
 SemanticLogger.add_appender(io: $stdout, formatter: :color)
 
 class Server < Sinatra::Base
-  get "/golang" do
-    "hootenanny"
+  set :port, (ENV["GOLANG_LATEST_PORT"] || 50005).to_i
+  enable :logging
+
+  get "/golang/" do
+    redirect "https://go.dev/", 303
+  end
+
+  not_found do
+    "404..."
+  end
+
+  error do
+    "error state..."
   end
 end
